9651072b5d4c35bf16566ded2ff53408f3b07328
Kilari Sharmila Naidu <kilarisharmila21@gmail.com>
2026-05-07 00:23:31 +0530

    A daily expenses calculator built using js.

---
+++ daily-expenses-calculator-advanced.js
@@ -0,0 +1,29 @@
+// User Details
+
+let userName = "Chinnu";
+
+// Daily Expenses
+
+let foodExpense = 120;
+let travelExpense = 60;
+let snacksExpense = 40;
+let otherExpense = 30;
+
+// Total Expense
+
+let totalExpense=foodExpense+travelExpense+snacksExpense+otherExpense;
+
+// Weekly Expense
+
+let weeklyExpense = totalExpense*7;
+
+// Monthly Expense
+
+let monthlyExpense = totalExpense*30;
+
+// Output
+
+console.log("User Name:", userName);
+console.log("Daily Expenses:", totalExpense);
+console.log("Weekly Expenses:", weeklyExpense);
+console.log("Monthly Expenses:", monthlyExpense);
